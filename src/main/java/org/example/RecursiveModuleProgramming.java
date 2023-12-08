b1af2930b3048318061623ad60f6f8aa30f10c39
Vlasosik <128188585+Vlasosik@users.noreply.github.com>
2023-12-08 19:07:06 +0200

    a recursive model of the Fibonacci task has been added

---
+++ src/main/java/org/example/RecursiveModuleProgramming.java
@@ -0,0 +1,27 @@
+package org.example;
+
+/**
+ * time complexity O(2^N)
+ * spatial complexity O(1)
+ * type UML Sequence Diagram
+ */
+public class RecursiveModuleProgramming {
+
+    public static void main(String[] args) {
+        int n = 10;
+        System.out.println("Fibonacci Series till " + n + " terms:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacciRecursion(i));
+            if (i < n - 1){
+                System.out.print(", ");
+            }
+        }
+    }
+
+    private static long fibonacciRecursion(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacciRecursion(n - 1) + fibonacciRecursion(n - 2);
+    }
+}
